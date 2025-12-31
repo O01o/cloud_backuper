f4006c8f799d5dd09de68bb59155d905e37df00a
Ayato Matsui <ayatomatsui010@gmail.com>
2025-12-31 07:46:51 +0000

    change sync_path.json path

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ from src.main.service import syncer as backup_service
 
 load_dotenv()
 
-local_path: str = os.path.join(".", "src")
-sync_path: str = os.path.join("project", "this")
-
 backup_s3_repository.init_bucket(bucket_name=os.getenv('BACKUP_BUCKET_NAME'))
 backup_service.init_syncer(backup=backup_s3_repository)
 backup_service.push_from_json(
